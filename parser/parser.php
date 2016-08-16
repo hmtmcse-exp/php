84532298f03217a5eb79622d7261c009e606d462
Touhid Mia <touhid@bitmascot.com>
2016-08-16 22:18:45 +0600

    Dynamic Form

---
+++ parser/parser.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: touhid
+ * Date: 16/08/2016
+ * Time: 2:19 PM
+ */
+
+$homepage = file_get_contents('template.blade.php');
+echo $homepage;
+
---
+++ parser/template.blade.php
@@ -0,0 +1,1 @@
+{{MISD::dynamicArtical()}}
