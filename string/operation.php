e3c15769bb6fbcc598bda282c3e84af126c87c40
Touhid Mia <hmtm.cse@gmail.com>
2016-08-27 00:57:50 +0600

    Random

---
+++ string/operation.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hmtmcse
+ * Date: 8/26/16
+ * Time: 2:38 PM
+ */
+
+$number = 100;
+echo "Length: " . strlen($number);
+
+$string = "Bangladesh";
+echo "<br> Substring " . substr($string,0,6);
